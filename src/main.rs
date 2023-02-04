136304f9435e0528f95526609d952e9ea4b08c79
Doomwhite <doomwhitex@gmail.com>
2023-02-03 21:38:22 -0300

    Finishes the book example

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,8 @@ struct Name(String);
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_startup_system(add_people)
-        .add_system(hello_world)
-        .add_system(greet_people)
+        .add_plugin(HelloPlugin)
         .run();
-}
-
-fn hello_world() {
-    println!("Hello World!");
 }
 
 fn add_people(mut commands: Commands) {
@@ -25,8 +19,23 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Zayna".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("hello {}!", name.0);
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("hello {}!", name.0);
+        }
+    }
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_startup_system(add_people)
+            .add_system(greet_people);
     }
 }
